ad2885afcea0d26afc993cd3f80f7ce637f540e1
Daffa <daffaikhsan23@gmail.com>
2023-12-04 16:55:32 +0700

    New updates

--- wordian_project/settings.py
+++ wordian_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'wordian_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wordiandev',
+        'USER': 'admindev',
+        'PASSWORD': 'Igeneration1',
+        'HOST': 'wordiandev.cb6arxiccopv.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
