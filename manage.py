9faca9f0eb479ae16d1c236c11c8bf99fc24248d
Daffa <daffaikhsan23@gmail.com>
2023-12-06 17:07:38 +0700

    Edit setting

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'wordian_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'wordian_project.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    # settings_module = 'wordian_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'wordian_project.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordian_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- wordian_project/wsgi.py
+++ wordian_project/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'wordian_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'wordian_project.settings'
+# settings_module = 'wordian_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'wordian_project.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordian_project.settings')
 
 application = get_wsgi_application()
