243d2b17dfe5ed1b6a7b10e5106632449bf39f9c
Daffa <daffaikhsan23@gmail.com>
2023-12-06 07:52:05 +0700

    Azure start

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wordian_project.settings')
+    settings_module = 'wordian_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'wordian_project.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- wordian_project/settings.py
+++ wordian_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--qae2d*o9-y(5@b@ggmf!%j3l!6r()5n(objb7r@)t!1)k9tn7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'wordian_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wordiandev',
-        'USER': 'admindev',
-        'PASSWORD': 'Igeneration1',
-        'HOST': 'wordiandev.cb6arxiccopv.ap-southeast-2.rds.amazonaws.com',
-        'PORT': '5432',
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
